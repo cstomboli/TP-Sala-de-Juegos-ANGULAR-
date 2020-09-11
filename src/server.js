175584cd316810fc72f5ef8cf14c1082b7aeee8c
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-11 01:44:32 -0300

    heroku6

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 constexpress = require('express');
 constapp = express();
 constpath = require('path');
-app.use(express.static(__dirname + '/TP-Sala-de-Juegos-ANGULAR-'));
+app.use(express.static(__dirname + '/tpsaladejuego'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) 
-{res.sendFile(path.join(__dirname + '/TP-Sala-de-Juegos-ANGULAR-/index.html'));});
+{res.sendFile(path.join(__dirname + '/tpsaladejuego/index.html'));});
 console.log(' successful!!');
