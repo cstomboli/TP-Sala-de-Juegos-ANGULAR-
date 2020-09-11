b98979b7d0f08dbcb74861fd6be17fb11a902cc9
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-11 19:18:59 -0300

    comillas simples al server.js

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
 const app = express();
+const path = require('path');
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/angularapp"));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/angularapp/index.html"));
-});
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
-console.log("Exitoso");
+app.get('/*', function(req, res) {
+res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+});
+console.log('Build successful!!');
