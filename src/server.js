7697ec27d8d255ead4aaa9655603c3767d614edc
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-11 17:12:08 -0300

    heroku11

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-constapp = express();
-constpath = require('path');
+const app = express();
+const path = require('path');
 app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) 
