5a41b295ec14dd3d66d88f89b1891a8868d1baa3
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-11 17:14:09 -0300

    a ver

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) 
 {res.sendFile(path.join(__dirname + '/angularapp/index.html'));});
-console.log('ok, successful!!');
+console.log('successful!!');
