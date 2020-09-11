c8b155585201f898e88b048f8c8692482f78b5c6
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-11 01:29:03 -0300

    heroku5

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/tpsaladejuego'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) 
 {res.sendFile(path.join(__dirname + '/tpsaladejuego/index.html'));});
-console.log('Build successful!!');
+console.log(' successful!!');
