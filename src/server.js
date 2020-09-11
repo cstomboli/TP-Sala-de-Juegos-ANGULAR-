d832281259f3bf09ce6238e7e060fabc65d39677
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-10 21:29:34 -0300

    herokuAnda?

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 constexpress = require('express');
 constapp = express();
 constpath = require('path');
-app.use(express.static(__dirname + '/sala-de-juegos'));
+app.use(express.static(__dirname + '/tpsaladejuego'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) 
-{res.sendFile(path.join(__dirname + '/sala-de-juegos/index.html'));});
+{res.sendFile(path.join(__dirname + '/tpsaladejuego/index.html'));});
 console.log('Build successful!!');
