3b12d9ceab11d699a9163856691967f691e7f284
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-11 17:23:39 -0300

    a ver herorku

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/angularapp'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/tpsaladejuego'));
+//app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) 
-{res.sendFile(path.join(__dirname + '/angularapp/index.html'));});
+{res.sendFile(path.join(__dirname + '/tpsaladejuego/index.html'));});
 console.log('successful!!');
